09edea920b490c3bda5006cfa06704adbeafbaa0
Kylaan <95552335+Kylaan@users.noreply.github.com>
2022-08-08 15:55:49 +0800

    Create Pi_FromHitting.py

---
+++ PythonVersion/Pi_FromHitting.py
@@ -0,0 +1,29 @@
+def TimeCounter (k):
+    cnt = 0
+    v0 = 0
+    u0 = -1
+    #start
+    u1 = (k-1)*u0 / (k+1)
+    v1 = k * (u0-u1)
+    #First hit
+    cnt+=1
+    v2 = 0
+    u2 = 0
+    while 1:
+        if (v1 < 0):
+            cnt+=1
+        if u1 > 0 and -v1 <= u1:
+            break
+        cnt+=1
+        u2=(k-1)*u1/(k+1)-2*v1/(k+1)
+        v2=2*k*u1/(k+1)+(k-1)*v1/(k+1)
+ 
+        v1 = v2
+        u1 = u2
+	
+    return cnt
+
+for i in range(1,10):
+    x = 10**(2*i)
+    print( "10^n , n=" , 2*i , ':')
+    print(TimeCounter(x))
